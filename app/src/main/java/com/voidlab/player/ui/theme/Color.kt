f8c2e7780300f296cee9f86f6345ae3f79192120
Ozrachjoe85 <ozrachjoe85@gmail.com>
2026-03-25 08:26:48 -0600

    Create Color.kt

---
+++ app/src/main/java/com/voidlab/player/ui/theme/Color.kt
@@ -0,0 +1,24 @@
+package com.voidlab.player.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Void Lab Color Palette
+val VoidBlack = Color(0xFF0A0A0A)
+val VoidDarkGray = Color(0xFF1A1A1A)
+val VoidGray = Color(0xFF2A2A2A)
+
+val VoidCyan = Color(0xFF00E5FF)
+val VoidCyanDark = Color(0xFF00B8D4)
+val VoidCyanLight = Color(0xFF62EFFF)
+
+val VoidPurple = Color(0xFF7C4DFF)
+val VoidPurpleDark = Color(0xFF651FFF)
+val VoidPurpleLight = Color(0xFF9965FF)
+
+val VoidPink = Color(0xFFFF4081)
+val VoidPinkDark = Color(0xFFF50057)
+val VoidPinkLight = Color(0xFFFF79B0)
+
+val VoidGreen = Color(0xFF00FF88)
+val VoidGreenDark = Color(0xFF00E676)
+val VoidGreenLight = Color(0xFF69FFAA)
