65a7c37be07c97b115ae0957437e74f476662879
Ozrachjoe85 <ozrachjoe85@gmail.com>
2026-03-25 14:10:32 -0600

    Update Color.kt

--- app/src/main/java/com/voidlab/player/ui/theme/Color.kt
+++ app/src/main/java/com/voidlab/player/ui/theme/Color.kt
@@ -3,22 +3,22 @@ package com.voidlab.player.ui.theme
 import androidx.compose.ui.graphics.Color
 
 // Void Lab Color Palette
-val VoidBlack = Color(0xFF0A0A0A)
-val VoidDarkGray = Color(0xFF1A1A1A)
-val VoidGray = Color(0xFF2A2A2A)
+val VoidBlack = Color(0xFF050508)
+val VoidBlackLight = Color(0xFF0A0A0F)
+val VoidBlackDark = Color(0xFF000000)
 
 val VoidCyan = Color(0xFF00E5FF)
-val VoidCyanDark = Color(0xFF00B8D4)
-val VoidCyanLight = Color(0xFF62EFFF)
+val VoidCyanLight = Color(0xFF6EFFFF)
+val VoidCyanDark = Color(0xFF00B2CC)
 
 val VoidPurple = Color(0xFF7C4DFF)
-val VoidPurpleDark = Color(0xFF651FFF)
-val VoidPurpleLight = Color(0xFF9965FF)
+val VoidPurpleLight = Color(0xFFB47CFF)
+val VoidPurpleDark = Color(0xFF3F1DCB)
 
 val VoidPink = Color(0xFFFF4081)
-val VoidPinkDark = Color(0xFFF50057)
 val VoidPinkLight = Color(0xFFFF79B0)
+val VoidPinkDark = Color(0xFFC60055)
 
 val VoidGreen = Color(0xFF00FF88)
-val VoidGreenDark = Color(0xFF00E676)
-val VoidGreenLight = Color(0xFF69FFAA)
+val VoidGreenLight = Color(0xFF5FFFB1)
+val VoidGreenDark = Color(0xFF00CC6A)
