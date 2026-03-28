45f2a56c0a3d2823854e7c93c65ffbdfb3e660d4
Ozrachjoe85 <ozrachjoe85@gmail.com>
2026-03-28 08:07:56 -0600

    Update and rename Color.kt to CYBERPUNK_Theme.kt

---
+++ app/src/main/java/com/voidlab/player/ui/theme/CYBERPUNK_Theme.kt
@@ -0,0 +1,29 @@
+package com.voidlab.player.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// PRIMARY NEON COLORS
+val NeonCyan = Color(0xFF00FFF0)
+val NeonMagenta = Color(0xFFFF006E)
+val NeonYellow = Color(0xFFFFFF00)
+val NeonPurple = Color(0xFFBB00FF)
+val NeonBlue = Color(0xFF0088FF)
+
+// BACKGROUND COLORS
+val DeepBlack = Color(0xFF0A0A0F)
+val DarkGray = Color(0xFF1A1A24)
+val MidGray = Color(0xFF252530)
+val LightGray = Color(0xFF35353F)
+
+// ACCENT COLORS
+val HotPink = Color(0xFFFF0080)
+val ElectricBlue = Color(0xFF00D9FF)
+val AcidGreen = Color(0xFF39FF14)
+val UVPurple = Color(0xFF9D00FF)
+
+// LEGACY COLORS (for compatibility)
+val VoidBlack = DeepBlack
+val VoidBlackLight = DarkGray
+val VoidCyan = NeonCyan
+val VoidPurple = NeonPurple
+val VoidPink = HotPink
